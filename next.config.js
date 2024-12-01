e77de2350fa0abeb16f83345d0ee27195e02e924
NisalWishwajithPeiris <2021is073@stu.ucsc.cmb.ac.lk>
2024-12-02 04:17:37 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-isProd = process.env.NODE_ENV === 'development';
+isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   basePath: isProd ? '/Inpower-Laptop' : '',
-  assetPrefix : isProd ? '/Inpower-Laptop' : '',
-  output : 'export',
   experimental: {
     appDir: true,
   },
- // set output directory as dist
   distDir: 'out', 
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
