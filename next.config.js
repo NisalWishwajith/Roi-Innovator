40c16f57ff2e2dbc8b4dd1f8cb8492c977626d2b
NisalWishwajithPeiris <2021is073@stu.ucsc.cmb.ac.lk>
2024-12-06 21:44:37 +0530

    created docker file

--- next.config.js
+++ next.config.js
@@ -3,18 +3,10 @@
 isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/Roi-Innovator' : '',
-  assetPrefix: isProd ? '/Roi-Innovator' : '',
   distDir: 'out' ,
-  output: 'export',
-  trailingSlash: true,
   experimental: {
     appDir: true,
-  },
-  reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  }
 };
 
 module.exports = nextConfig;
