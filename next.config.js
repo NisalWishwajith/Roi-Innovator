c50de35342b7503c4c0f9c5af5edada0d7ab5123
NisalWishwajith <wishwajithnisal@gmail.com>
2024-12-29 21:48:15 +0530

    changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true,
+    assetPrefix: isProd ? "/Roi-Innovator/" : ""
+  },
 };
 
 module.exports = nextConfig;
